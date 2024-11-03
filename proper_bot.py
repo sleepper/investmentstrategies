6c83fdbdbb29881ff5542151aae9037faa8b5ac8
Aleksei <kek@mail.ru>
2024-11-03 17:11:00 +0100

    Tried to fix the menu, but did not make it really

--- proper_bot.py
+++ proper_bot.py
@@ -1,19 +1,48 @@
+import pandas as pd
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from telegram import Update, BotCommand
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, Updater
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+#https://servat.medium.com/creating-a-telegram-bot-with-python-4a3b4906c101
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, this bot is your personal financial advisor with all the relevant disclaimers")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="This is how the bot works") #TODO Add HTML text here
+
+async def send_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_says = context.args
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = f"Below you will find the business description for {user_says}. \nbla-bla-bla")
+
+async def send_table(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = {
+            'Name':['Alex','Alexander'],
+            'Age':[1,31]
+        }
+    df_str = pd.DataFrame(data).to_string(index=False)
+    df_str_tm = '<pre>' + df_str + '</pre>'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = df_str_tm, parse_mode='HTML')
+
+async def send_picture(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    str_path = 'profiles\ADBE\_stats_profile.png'
+    await context.bot.send_document(chat_id=update.effective_chat.id, document = open(str_path,'rb'))
 
 if __name__ == '__main__':
+    
     application = ApplicationBuilder().token('1470074102:AAEd4en4YhzADfW2mgMb7m4Xp0_HHKoD_qw').build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help))
+    application.add_handler(CommandHandler('text', send_text))
+    application.add_handler(CommandHandler('table', send_table))
+    application.add_handler(CommandHandler('picture', send_picture))
+
+    #application.bot.set_my_commands([BotCommand('start','start_text'),BotCommand('help','help_text'),BotCommand('text','text_text')])
+
     application.run_polling()
--- send_table.py
+++ send_table.py
@@ -4,6 +4,8 @@ import telegram
 
 import pandas as pd
 
+from telegram import BotCommand
+
 data = {
         'Name':['Alex','Alexander'],
         'Age':[1,31]
@@ -15,6 +17,7 @@ df = '<pre>' + df + '</pre>'
 async def main():
     bot = telegram.Bot('1470074102:AAEd4en4YhzADfW2mgMb7m4Xp0_HHKoD_qw')
     async with bot:
+        await bot.set_my_commands([BotCommand('start','start_text'),BotCommand('help','help_text'),BotCommand('text','text_text'),BotCommand('picture','picture_text')])
         await bot.send_message(text=df, chat_id = '145125445', parse_mode='HTML')
         print(df)
 
